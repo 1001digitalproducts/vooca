841d5dc500d5516ec138354aa273848617c6df3e
rroossyyiidd <rroossyyiidd@gmail.com>
2018-01-31 16:53:44 +0700

    test button

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Content } from 'native-base';
 
 export default class App extends React.Component {
 
@@ -13,7 +14,13 @@ export default class App extends React.Component {
     render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Test button</Text>
+          <Content>
+              // NativeBase default style
+              <Button>
+                  <Text>Click Me! </Text>
+              </Button>
+          </Content>
       </View>
     );
   }
